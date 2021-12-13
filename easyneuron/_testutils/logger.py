34d14916985a2f907333588ba3a176fd8c780132
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-12-13 09:00:23 +0000

    🐛 FIX: bug with tests

--- easyneuron/_testutils/logger.py
+++ easyneuron/_testutils/logger.py
@@ -1,17 +1,22 @@
+import os
+
 from easyneuron._logging import get_logger
 
 
 def log_errors(func):
     def wrapper(*args, **kwargs):
-        logger = get_logger("logs/" + func.__module__ + ".log")
+        if os.environ.get("GITHUB_ACTIONS") not in ["true", "True", "TRUE", True]:
+            logger = get_logger("logs/" + func.__module__ + ".log")
 
-        try:
+            try:
+                func(*args, **kwargs)
+                logger.info(f"{func.__qualname__} - Test Success")
+            except Exception as e:
+                message = " ".join(str(i) for i in e.args)
+                logger.error(
+                    f"\"{str(type(e))[8:-2]}\" @ {func.__qualname__} - {message}")
+                raise e
+        else:
             func(*args, **kwargs)
-            logger.info(f"{func.__qualname__} - Test Success")
-        except Exception as e:
-            message = " ".join(str(i) for i in e.args)
-            logger.error(
-                f"\"{str(type(e))[8:-2]}\" @ {func.__qualname__} - {message}")
-            raise e
 
     return wrapper
