c2656a7352798fbddc5333c6cce8a2ba39189712
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-12-18 19:15:25 +0000

    👌 IMPROVE: update examples

--- examples/kNN.py
+++ examples/kNN.py
@@ -1,8 +1,11 @@
+from easyneuron.data.gen import gen_stairs
 from easyneuron.neighbours import KNNClassifier
-from easyneuron.data import gen_stairs
+from easyneuron.metrics import accuracy
 
-X, y = gen_stairs()
+X, y = gen_stairs(3, 2, sd=0.1, samples=1000)
 
 model = KNNClassifier()
-model.fit(X, y)
-model.predict(X)
+model.fit(X[:900], y[:900])
+preds = model.predict(X[900:])
+print(preds)
+print(accuracy(preds, y[900:]))
